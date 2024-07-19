c55733b289eacfa21f13d2699a1cb2bab80e6a69
zenitsu <22ucs129@lnmiit.ac.in>
2024-07-20 02:02:58 +0530

    Added Search Dialog

--- client/src/components/specific/Search.jsx
+++ client/src/components/specific/Search.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
-import { Dialog } from "@mui/material"
+import React, { useState } from 'react'
+import { Dialog, DialogTitle, InputAdornment, List, ListItem, ListItemText, Stack, TextField } from "@mui/material"
+import { useInputValidation } from "6pp"
+import { Search as SearchIcon } from "@mui/icons-material"
+import UserItem from '../shared/UserItem';
+import { sampleUsers } from '../../constants/sampleData';
 
 function Search() {
+
+  const search = useInputValidation("")
+
+  const [users,setUsers] = useState(sampleUsers);
+
+  const addFriendHandler = (id) => {
+    console.log(id);
+  }
+
+  let isLoadingSendFriendRequest = false;
+
   return (
     <Dialog open>
-      Search
+      <Stack p={"2rem"} direction={"column"} width={"25rem"}>
+        <DialogTitle textAlign={"center"}>Find People</DialogTitle>
+        <TextField 
+          label=""
+          value={search.value}
+          onChange={search.changeHandler}
+          variant='outlined'
+          size='small'
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <SearchIcon />
+              </InputAdornment>
+            )
+          }}
+        />
+
+        <List>
+          {
+            users.map((i) => (
+              <UserItem 
+                user={i} 
+                key={i._id} 
+                handler={addFriendHandler} 
+                handlerIsLoading={isLoadingSendFriendRequest}
+              />
+            ))
+          }
+        </List>
+
+      </Stack>
     </Dialog>
   )
 }
--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -28,3 +28,16 @@ export const sampleChats = [
         members: ["3","4"],
     }
 ]
+
+export const sampleUsers = [
+    {
+        avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+        name:"John Doe",
+        _id: "1",
+    },
+    {
+        avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+        name:"John Boi",
+        _id: "2",
+    },
+]
