00547f4c067a3fe39d690f6f2fd2e5790aeb59ab
zenitsu <22ucs129@lnmiit.ac.in>
2024-07-23 04:52:02 +0530

    Added Admin Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,9 @@ const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Chat = lazy(() => import("./pages/Chat"));
 const Groups = lazy(() => import("./pages/Groups"));
-const NotFound = lazy(() => import("./pages/NotFound"))
+const NotFound = lazy(() => import("./pages/NotFound"));
+const AdminLogin = lazy(() => import("./pages/admin/AdminLogin"));
+const Dashboard = lazy(() => import("./pages/admin/Dashboard"));
 
 let user = true;
 
@@ -29,6 +31,9 @@ const App = () => {
             </ProtectRoute>
           }
           />
+
+          <Route path='/admin' element={<AdminLogin/>} />
+          <Route path='/admin/dashboard' element={<Dashboard/>} />
 
           <Route path='*' element={<NotFound/>}/>
           
